55bb7eda2a21dab92608079b2fabc21ea3926911
aittamlihat <107299077+aittamlihat@users.noreply.github.com>
2022-06-11 09:59:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ def run_query(query):
         cur.execute(query)
         return cur.fetchall()
 
-rows = run_query("SELECT * from mytable;")
+rows = run_query("SELECT * from sensor;")
 
 # Print results.
 for row in rows:
